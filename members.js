17769700fc010d97d27021464788c9a542c76895
Arnav Sirigere <arnavsirigere@gmail.com>
2021-01-19 09:17:55 +0530

    :sparkles: Adding my random numebr to json file

--- members.js
+++ members.js
@@ -1,6 +1,7 @@
 const members = `
   [
     { "name": "Daniel Granger", "random": 1.137 },
-    { "name": "Hunter Parks", "random": 49080 }
+    { "name": "Hunter Parks", "random": 49080 },
+    { "name": "Arnav Sirigere", "random": 28461 },
   ]
 `;

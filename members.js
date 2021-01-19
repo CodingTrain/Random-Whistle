d2125ee6b6fc1238c23c4ab53989bad71bb3dc52
Arnav Sirigere <arnavsirigere@gmail.com>
2021-01-19 09:26:37 +0530

    :bug: Fixing json syntax

--- members.js
+++ members.js
@@ -2,6 +2,6 @@ const members = `
   [
     { "name": "Daniel Granger", "random": 1.137 },
     { "name": "Hunter Parks", "random": 49080 },
-    { "name": "Arnav Sirigere", "random": 28461 },
+    { "name": "Arnav Sirigere", "random": 28461 }
   ]
 `;

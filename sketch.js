39b31fe7e8b78a960407dd2c8167f2fc4f9eb758
kobeliesenborgs <kobe.liesenborgs1998@gmail.com>
2023-01-17 00:18:57 +0100

    Secret Rainbow, press up arrow

--- sketch.js
+++ sketch.js
@@ -1,17 +1,15 @@
 let x;
 let y;
+let seed;
 
 function setup() {
   const canvas = createCanvas(800, 500);
   canvas.parent('p5canvas');
-  background(0);
-  x = width / 2;
-  y = height / 2;
-
+  rainbow=false;
   let params = getURLParams();
   console.table(params);
   let name = params.name || 'Choo Choo';
-  let seed = params.seed || '12345';
+  seed = params.seed || '12345';
   randomSeed(int(seed));
 
   select('#thanks').html(
@@ -20,10 +18,31 @@ function setup() {
   select('#walk').html(
     `Enjoy this unique random walk with your personalized seed of ${seed}!`
   );
+  resetSketch();
+}
+
+function toggleRainbow(){
+  rainbow = !rainbow;
+}
+
+function keyPressed(){
+  print(keyCode)
+  if (keyCode === UP_ARROW || keyCode == 38){
+    toggleRainbow();
+    resetSketch();
+  }
 }
 
 function draw() {
-  stroke(255);
+  colorMode(HSB);
+  if(!rainbow){
+    stroke(360,0,100);
+  }
+
+  else{
+    if(frameCount%5==0){hueState = ((hueState+1)%360)};
+    stroke(hueState,100,100);
+  }
   strokeWeight(1);
   point(x, y);
   const r = int(random(4));
@@ -41,4 +60,11 @@ function draw() {
       y = y - 1;
       break;
   }
+}
+function resetSketch(){
+  background(0);
+  x = width / 2;
+  y = height / 2;
+  hueState=0;
+  randomSeed(int(seed));
 }
